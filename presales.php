ac5f4bc57332d22a1b6d3f0eddb77f0db0779f30
devbharat2k <devbharath21k@gmail.com>
2023-07-27 18:55:39 +0530

    new changes

--- presales.php
+++ presales.php
@@ -6,15 +6,20 @@ $con = mysqli_connect("srv787.hstgr.io", "u842521168_devbharath", "Admin@123", "
 if (isset($_POST['submit'])) {
 
     // Fetching and storing the form data in variables
-    $fname = $con->real_escape_string($_POST['Fname']);
-    $lname = $con->real_escape_string($_POST['lname']);
-    $email = $con->real_escape_string($_POST['email']);
-    $mobile = $con->real_escape_string($_POST['mobile']);
-    $message = $con->real_escape_string($_POST['message']);
+    $Fname = $con->real_escape_string($_POST['Fname']);
+    $Lname = $con->real_escape_string($_POST['Lname']);
+    $Email = $con->real_escape_string($_POST['Email']);
+    $Mobile = $con->real_escape_string($_POST['Mobile']);
+    $Tech_Product1 = $con->real_escape_string($_POST['Tech_Product1']);
+    $Tech_Product2 = $con->real_escape_string($_POST['Tech_Product2']);
+    $Tech_Product3 = $con->real_escape_string($_POST['Tech_Product3']);
+    $Tech_Product4 = $con->real_escape_string($_POST['Tech_Product4']);
+    $Message1 = $con->real_escape_string($_POST['Message1']);
+
 
     // Query to insert the variable data into the database
-    $sql = "INSERT INTO axess_technology (fname, lname, email, mobile,) 
-            VALUES ('', '', '', '', '', '', '', '', '', '', '', '', '', '')";
+    $sql = "INSERT INTO axess_technology (Fname, Lname, Email, Mobile, Tech_Product1, Tech_Product2, Tech_Product3, Tech_Product4 ,Message1,)
+            VALUES ('$Fname', '$Lname', '$Email', '$Mobile','$Tech_Product1','$Tech_Product2',$Tech_Product3',$Tech_Product4', '$Message1')";
 
     // Execute the query and return a message
     if (!$result = $con->query($sql)) {
